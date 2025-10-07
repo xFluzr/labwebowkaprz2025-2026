ed82eb549cac0ca654263cf55b6b2aaa93b86262
Kuba <jakub.jaszcz538@gmail.com>
2025-10-07 09:38:06 +0200

    Zadanie pierwsze lab 1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-(function () {
-  //TODO
-})();
+const btn1= document.querySelector("#ex1_button");
+const content1= document.querySelector("#ex1_content");
+
+btn1.addEventListener("click",()=>{
+  const numberArray=[];
+  for(let i=0;i<10;i++){
+    numberArray.push(i);
+  }
+  content1.innerHTML=numberArray;
+})
